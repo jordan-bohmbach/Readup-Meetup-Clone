47ceef6480339e3357feb76e16c23d6e3d9c2f31
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-08-12 17:16:51 -0500

    completed the login router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,6 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter)
 
 router.get('/hello/world', function (req, res) {
-    console.log(req)
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
 });
