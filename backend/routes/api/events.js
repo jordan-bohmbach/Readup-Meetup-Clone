399d8a39606666c6c010ee22be8198022c1bab7c
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-08-18 15:17:19 -0500

    started the events post route

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,10 +1,34 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
-const {Event} = require('../../db/models')
+const {Event, Venue, Group} = require('../../db/models')
 
 router.get('/', asyncHandler(async (req, res, next) => { 
-    const eventList = await Event.findAll()
+    const eventList = await Event.findAll({
+        include: [Venue, Group]
+    })
     return res.json(eventList)
+}))
+
+router.post('/', asyncHandler(async (req, res, next) => {
+    const {
+        name,
+        date,
+        capacity,
+        image,
+        venue,
+        category
+    } = req.body;
+
+    const newEvent = await Event.Create({
+        name,
+        date,
+        capacity,
+        image,
+        venue,
+        category
+    })
+    console.log('newEvent = ', newEvent)
+    return newEvent;
 }))
 
 module.exports = router
--- frontend/src/components/EventListPage/index.js
+++ frontend/src/components/EventListPage/index.js
@@ -1,22 +1,43 @@
 import { Link } from "react-router-dom"
 import EventTile from "../EventTile"
 import './EventListPage.css'
+import {React, useState, useEffect} from "react"
+import EventDetails from "../EventDetails"
 
 const EventListPage = ({eventList}) => {
+    const [sideOpen, setSideOpen] = useState(true)
+    const [selectedEvent, setSelectedEvent] = useState('')
+
+    useEffect(() => {
+        if(selectedEvent) setSideOpen(true)
+    }, [selectedEvent])
+
+    useEffect(()=> {
+        if(!sideOpen) setSelectedEvent('')
+    }, [sideOpen])
+
     return(
         <div className='event-page'>
             <h1>Events</h1>
-            <div className='event-list'>
-                {eventList.map(event =>
-                    <EventTile
-                        key={event.id}
-                        event={event}
-                    />
-                )}
+            <div className='event-page-upper-section'>
+                <div className='event-list'>
+                    {eventList.map(event =>
+                        <EventTile
+                            key={event.id}
+                            event={event}
+                        />
+                    )}
+                </div>
+                <div className="event-side-panel">
+                    <div className="event-side-panel-toggle-wrapper">
+                        <div className="event-side-panel-toggle"
+                            onClick={() => setSideOpen(!sideOpen)}>
+                            {sideOpen ? '>' : '<'}
+                        </div>
+                    </div>
+                    <EventDetails visible={sideOpen} event={selectedEvent} />
+                </div>
             </div>
-            <Link to='events/new' className='create-event-link'>
-                <span className='create-event-button'>Create a new Event</span>
-            </Link>
         </div>
     )
 }
