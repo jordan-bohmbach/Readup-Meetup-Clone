2fc94a5b63795b2029a7a8fe7112093c735b97a9
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-10-19 11:28:17 -0500

    started implementing the search feature

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import * as sessionActions from "../../store/session";
 import './Navigation.css';
+import { Link } from 'react-router-dom';
 
 function Navigation({ isLoaded }) {
     const dispatch = useDispatch()
     const sessionUser = useSelector(state => state.session.user);
+    const [searchWord, setSearchWord] = useState('')
+    const [searchingClickOut, setSearchingClickOut] = useState(false)
+    const [filteredEvents, setFilteredEvents] = useState([])
+    const eventList = useSelector(state=>Object.values(state.events))
 
     const handleDemoUser = () => {
         return dispatch(sessionActions.login({credential: 'Demo-lition', password: 'password'}))
@@ -29,6 +34,26 @@ function Navigation({ isLoaded }) {
         );
     }
 
+    const handleTypeing = (e) => {
+        setSearchWord(e.target.value)
+    }
+
+    const handleBlur = () => {
+        console.log('bluring here')
+        setTimeout(() => setSearchingClickOut(true), 200)
+    }
+
+    const handleClick = () => {
+        setSearchingClickOut(false)
+    }
+
+    useEffect(() => {
+        setFilteredEvents(eventList.filter(event => event.name.toLowerCase().includes(searchWord.toLowerCase())))
+        console.log('searchWord is now ', searchWord)
+        console.log('eventList is now ', eventList)
+        console.log('filteredEvents is now ', filteredEvents)
+    }, [searchWord])
+
     return (
         <ul>
             <li>
@@ -36,10 +61,25 @@ function Navigation({ isLoaded }) {
                     <img src='/images/nav-logo.png' alt='img not found'></img>
                 </NavLink>
                 <div className='search-container'>
-                    <input className='search-input' defaultValue='search for keywords'></input>
-                    <input className='location-input' defaultValue='Chicago, IL'></input>
-                    <button className='navigation-search-button'>Search</button>
+                    <input
+                        type='text'
+                        name='search'
+                        value={searchWord}
+                        placeholder='Search for Events'
+                        onChange={handleTypeing}
+                        onBlur={handleBlur}
+                        onClick={handleClick}>
+
+                    </input>
+                    <div className='outer-search-results-container'>
+                        {(filteredEvents && searchWord) ? <ul className={searchingClickOut ? 'invisible-search' : 'search-results-container'}>
+                            {filteredEvents?.map(event => (
+                                <li key={event.id}><Link to={`/events/${event.id}`} >{event.name}</Link></li>
+                            ))}
+                        </ul> : ''}
+                    </div>
                 </div>
+                <button className='navigation-search-button'>Search</button>
                 {isLoaded && sessionLinks}
             </li>
         </ul>
