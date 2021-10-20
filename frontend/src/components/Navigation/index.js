36e9f0d11ebd4647ee76dfaf8efc86a9de2d88d6
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-10-20 11:19:50 -0500

    implemented search for events and groups

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -13,7 +13,9 @@ function Navigation({ isLoaded }) {
     const [searchWord, setSearchWord] = useState('')
     const [searchingClickOut, setSearchingClickOut] = useState(false)
     const [filteredEvents, setFilteredEvents] = useState([])
+    const [filteredGroups, setFilteredGroups] = useState([])
     const eventList = useSelector(state=>Object.values(state.events))
+    const groupList = useSelector(state=>Object.values(state.groups))
 
     const handleDemoUser = () => {
         return dispatch(sessionActions.login({credential: 'Demo-lition', password: 'password'}))
@@ -49,40 +51,51 @@ function Navigation({ isLoaded }) {
 
     useEffect(() => {
         setFilteredEvents(eventList.filter(event => event.name.toLowerCase().includes(searchWord.toLowerCase())))
+        setFilteredGroups(groupList.filter(group=>group.type.toLowerCase().includes(searchWord.toLowerCase())))
         console.log('searchWord is now ', searchWord)
         console.log('eventList is now ', eventList)
         console.log('filteredEvents is now ', filteredEvents)
+        console.log('groupList is now ', groupList)
+        console.log('filteredGroups is now ', filteredGroups)
     }, [searchWord])
 
     return (
-        <ul>
-            <li>
+        <div className='outermost-navbar-container'>
+            <div className='lower-navbar-container'>
                 <NavLink exact to="/">
                     <img src='/images/nav-logo.png' alt='img not found'></img>
                 </NavLink>
-                <div className='search-container'>
-                    <input
-                        type='text'
-                        name='search'
-                        value={searchWord}
-                        placeholder='Search for Events'
-                        onChange={handleTypeing}
-                        onBlur={handleBlur}
-                        onClick={handleClick}>
+                <div className='search-and-button-container'>
+                    <div className='search-container'>
+                        <input
+                            className='search-input'
+                            type='text'
+                            name='search'
+                            value={searchWord}
+                            placeholder='Search for Events or Groups'
+                            onChange={handleTypeing}
+                            onBlur={handleBlur}
+                            onClick={handleClick}>
 
-                    </input>
-                    <div className='outer-search-results-container'>
-                        {(filteredEvents && searchWord) ? <ul className={searchingClickOut ? 'invisible-search' : 'search-results-container'}>
-                            {filteredEvents?.map(event => (
-                                <li key={event.id}><Link to={`/events/${event.id}`} >{event.name}</Link></li>
-                            ))}
-                        </ul> : ''}
+                        </input>
+                        <div className='outer-search-results-container'>
+                            {(filteredEvents && searchWord) ? <ul className={searchingClickOut ? 'invisible-search' : 'search-results-container'}>
+                                {filteredEvents.length ? <li className='search-spacer'>Matching Events</li> : ''}
+                                {filteredEvents?.map(event => (
+                                    <li key={event.id}><Link to={`/events/${event.id}`} >{event.name}</Link></li>
+                                ))}
+                                {filteredGroups.length ? <li className='search-spacer'>Matching Groups</li> : ''}
+                                {filteredGroups?.map(group=>(
+                                    <li key={group.id}><Link to={`/groups/${group.id}`}>{group.type}</Link></li>
+                                ))}
+                            </ul> : ''}
+                        </div>
                     </div>
+                    <button className='navigation-search-button'>Search</button>
                 </div>
-                <button className='navigation-search-button'>Search</button>
                 {isLoaded && sessionLinks}
-            </li>
-        </ul>
+            </div>
+        </div>
     );
 }
 
